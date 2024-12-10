6aa13e6111279e2e8f844e0c32515f76c186a9bb
Elitezen <dev.alejandromarin@gmail.com>
2024-12-09 20:24:58 -0500

    Responsive fixes

--- src/pages/home/components/showcase/index.tsx
+++ src/pages/home/components/showcase/index.tsx
@@ -66,7 +66,7 @@ function Showcase() {
                     <Button
                         iconURL={twitterXIcon}
                         href="https://x.com/cylipsonrblx?lang=en"
-                        label="Twitter / X"
+                        label="X.com"
                         style={{
                             backgroundColor: '#000',
                             color: '#FFF',
