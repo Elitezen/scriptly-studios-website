b793fe2dd8e3d89f2f9cf984bfcf0c2f32664118
Elitezen <dev.alejandromarin@gmail.com>
2024-12-14 13:40:49 -0500

    Added Event section

---
+++ src/pages/home/components/Event/index.tsx
@@ -0,0 +1,40 @@
+import styles from "./index.module.scss";
+
+import eventImg from "../../../../assets/images/wallpaper/flag-wars-xmas-event.png";
+
+function Event() {
+    return (
+        <section className={styles.event}>
+            <div className={styles.contentContainer}>
+                <div className={styles.imageSide}>
+                    <img src={eventImg} alt="Flag Wars Christmas Event Art" />
+                </div>
+
+                <div className={styles.textSide}>
+                    <div className={styles.text}>
+                        <h3>New Season 3 Update for Flag Wars!</h3>
+                        <p>
+                            Get festive with the new Christmas update for Flag Wars!
+                            <ul>
+                                <li>⚔️ 2 new weapons</li>
+                                <li>🗺️ New Santas Workshop map</li>
+                                <li>👑 New Royal skin bundle</li>
+                                <li>🕶️ +50 new cosmetics</li>
+                                <li>✨ Map Remasters</li>
+                                <li>🔧 Balance Changes</li>
+                            </ul>
+                        </p>
+                    </div>
+
+                    <div className={styles.buttonContainer}>
+                        <a href="#" rel="external">
+                            <button>Learn More</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Event;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,6 +4,7 @@ import Community from "./components/community";
 import Youtube from "./components/youtube";
 import Games from "./components/games";
 import Showcase from "./components/showcase";
+import Event from "./components/Event";
 // import styles from "./index.module.scss";
 
 function Home() {
@@ -12,6 +13,7 @@ function Home() {
             <Header />
             <Showcase />
             <Games />
+            <Event />
             <Community />
             <Youtube />
             <Footer />
